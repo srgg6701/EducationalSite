64e5b415f0159bbfee8e58d694a436cc5abbda93
Liz <zodiacweight@gmail.com>
2015-01-07 16:52:41 +0300

    Осталось:
    1. Разделить крупный модуль на части;
    1. Глоссарий;
    2. Тестовые задания;
    3. Перенести список используемых источников;
    4. Прочие поправки.

---
+++ contents/test.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Документ без названия</title>
+</head>
+<body>
+<?php	
+ob_start();
+?>
+<h1>Hello!</h1>
+Remember me?
+<?php 
+$html = ob_get_contents();
+ob_end_clean();
+?>
+<hr>
+<?php	echo $html;
+?>
+</body>
+</html>
