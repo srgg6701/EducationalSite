b595786386d72ec7d1373a4b34c749ff1039e3e2
srgg6701 <srgg67@gmail.com>
2015-01-10 22:22:55 +0300

    Установлен обработчик результатов для текстовых полей.

--- static/js/tests.js
+++ static/js/tests.js
@@ -1,41 +1,9 @@
+// создать псевдокласс с геттерами и сеттерами
 var answ = new manageRightAnswers();
 answ.setAnswers([]);
-
-$(function(){
-    var wrong="<div class='wrong'><span class='text-red'>Обнаружены ошибки</span> [<span class='pseudolink' onclick='showRight()'>показать правильные ответы</span>]</div>",
-        correct="<div class='correct'>Всё верно!</div>";
-
-    $('.check').on('click', function(){
-        var answers=[],
-            errors=0;
-
-        $('select').each(function(){
-            var right=$('option[value="true"]',this).text(),
-                selected=$(this).find('option:selected').val();
-            answers.push(right);
-            if(selected!='true') errors+=1;
-        });
-
-        // сохранить правильные ответы
-        answ.setAnswers(answers);
-
-        var btn_check =  $('.check'),
-            checkResults = function(){
-                console.log('str 24');
-                removeResults();
-                if(errors){
-                    console.log('errors');
-                    $(btn_check).after(wrong);
-                }else{
-                    console.log('no errors');
-                    $(btn_check).after(correct);
-                }
-                $(btn_check).after(' <span id="clear-results" class="pseudolink" onclick="removeResults()">Сбросить результаты</span>');
-        };
-        checkResults();
-    });
-});
-// сохранить и извлечь правильные ответы
+/**
+ * Блок для сохраннения и извлечения правильных ответов
+ */
 function manageRightAnswers(){
     var answers=[];
     return{
@@ -48,12 +16,19 @@ function manageRightAnswers(){
         }
     }
 }
+/**
+ * Удалить блоки с результатами
+ */
 function removeResults(){
     $('.correct').remove();
     $('.wrong').remove();
     $('#clear-results').remove();
     $('#right-results').remove();
 }
+/**
+ * Показать правильные ответы
+ * Вызывается кликом по ссылке "показать правильные ответы"
+ */
 function showRight(){
     var answers_block='<div id="right-results" class="box">';
     $(answ.getAnswers()).each(function(index,element){
@@ -61,4 +36,45 @@ function showRight(){
     });
     answers_block+='</div>';
     $('.wrong').after(answers_block);
+}
+//-------------------------------
+/**
+ * Проверить результаты, выбранные из списков
+ */
+function checkSelects(){
+    var answers=[], errors=0;
+    $('select').each(function(){
+        var right=$('option[value="true"]',this).text(),
+            selected=$(this).find('option:selected').val();
+        answers.push(right);
+        if(selected!='true') errors+=1;
+    });
+    // построить блок с результатами
+    buildResults(answers,errors);
+}
+/**
+ * Проверить ответ (текстовое поле)
+ */
+function checkFieldValue(value){
+    var answer=$.trim($('#answer-field').val().toLowerCase());
+    //console.log('answer: '+answer+', value: '+value);
+    buildResults([value],(answer==value)? 0:1);
+}
+/**
+ * Построить результаты
+ * @param answers - все правильные ответы
+ * @param errors - счётчик ошибок
+ */
+function buildResults(answers,errors){
+    var wrong="<div class='wrong'><span class='text-red'>Обнаружены ошибки</span> [<span class='pseudolink' onclick='showRight()'>показать правильные ответы</span>]</div>",
+        correct="<div class='correct'>Всё верно!</div>",
+        // Кнопка, по которой клацали для проверки
+        btn_check =  $('.check');
+    // сохранить правильные ответы
+    answ.setAnswers(answers);
+    removeResults(); //console.log('str 24');
+    // Определитьс с корректностью результата и отобразить его
+    $(btn_check).after((errors)? wrong:correct);
+    // и отобразить!
+    $(btn_check).after(' <span id="clear-results" class="pseudolink" onclick="removeResults()">Сбросить результаты</span>');
 }
